499e0b7d79b72ba6f6b876b69c03c134d1b5497b
Michael Chew <michael.chew@actifio.com>
2019-12-11 20:26:11 +1100

    Update actrun.ps1

--- actrun.ps1
+++ actrun.ps1
@@ -18,8 +18,16 @@ if (! $env:ACTSESSIONID ){
    Connect-Act $ActIP -actuser $ActUser -passwordfile $TmpPasswdFile -ignorecerts | Out-Null
 }
 
-udsinfo lsversion | select component, version
-udsinfo lsversion | select installed, component, version
+$verlist = udsinfo lsversion
+foreach ($verid in $verlist) {
+  write-output "comp = $verid.component, ver = $verid.version"
+}
+
+$verlist2 = udsinfo lsversion
+foreach ($verid2 in $verlist) {
+  write-output "installed = $verid2.installed, ver = $verid2.version"
+}
+
 udsinfo lscluster
 
 if (! $env:ACTSESSIONID ){
