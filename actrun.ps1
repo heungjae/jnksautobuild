3788a0ea27394528f77d0345e82842be1c81d160
Michael Chew <michael.chew@actifio.com>
2019-12-11 18:42:26 +1100

    Add files via upload

---
+++ actrun.ps1
@@ -0,0 +1,29 @@
+param(
+[string] $ActIP),
+[string] $actuser),
+[string] $ActPass))
+
+
+$env:IGNOREACTCERTS = $true
+$LocalTempDir = "c:\temp\"
+If(!(test-path $LocalTempDir)) {
+    New-Item -ItemType Directory -Force -Path $LocalTempDir | out-null
+    }
+    
+$TmpPasswdFile = "$LocalTempDir\$env:USERNAME-passwd.key"
+
+"$ActPass" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File $TmpPasswdFile
+
+if (! $env:ACTSESSIONID ){
+   Connect-Act $ActIP -actuser $ActUser -passwordfile $TmpPasswdFile -ignorecerts | Out-Null
+}
+
+udsinfo lsversion
+reportpools
+
+if (! $env:ACTSESSIONID ){
+  write-warning "Login to VDP $ActIP failed"
+  break
+} else {   
+  Disconnect-Act | Out-Null
+} 
